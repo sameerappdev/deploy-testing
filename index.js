0a00c0e9af1d7aff6dd65a8f8ab7e89a94ee5b14
sameerappdev <sameer.mmohsin@gmail.com>
2024-06-21 17:59:11 +0500

    cloning

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
+// require('dotenv').config()
+// const express = require("express");
+// const app = express();
+// const port = 4000;
+// const PORT_NO = process.env.PORT
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.get('/google', (req, res) => {
+//   res.send('Google');
+// });
+
+// app.get('/login', (req, res) => {
+//   res.send('<h1>Please Login</h1>');
+// });
+
+// app.get('/youtube', (req, res) => {
+//   res.send('<h2>Youtube</h2>');
+// });
+
+// app.listen(PORT_NO, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
 require('dotenv').config()
-const express = require("express");
-const app = express();
-const port = 4000;
 const PORT_NO = process.env.PORT
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+let records = [];
+
+
+router.get('/', (req, res) => {
+  res.send('App is running..');
 });
 
-app.get('/google', (req, res) => {
-  res.send('Google');
+router.post('/add', (req, res) => {
+  res.send('New record added.');
 });
 
-app.get('/login', (req, res) => {
-  res.send('<h1>Please Login</h1>');
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
 });
 
-app.get('/youtube', (req, res) => {
-  res.send('<h2>Youtube</h2>');
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
 });
+
+
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Aayush',
+    },
+    {
+      id: '002',
+      name: 'rohit',
+    },
+    {
+      id: '003',
+      name: 'Mohit',
+    },
+  ]);
+});
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
 
 app.listen(PORT_NO, () => {
   console.log(`Example app listening on port ${port}`);
